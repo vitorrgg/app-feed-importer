67c53b73f3025bb3d08c653770405b37b8c28a4a
GankSir <sirgank13@gmail.com>
2021-11-22 17:39:27 -0300

    add basic parser on feed product

--- functions/lib/gmc-to-ecom.js
+++ functions/lib/gmc-to-ecom.js
@@ -11,7 +11,15 @@ const parseProduct = async (feedProduct) => {
       subtitle: getFeedValueByKey('subtitle', feedProduct),
       meta_title: getFeedValueByKey('title', feedProduct),
       meta_description: getFeedValueByKey('description', feedProduct),
-      keywords: (getFeedValueByKey('google_product_category', feedProduct) || '').split('&gt;').map(x => x.trim())
+      keywords: (getFeedValueByKey('google_product_category', feedProduct) || '').split('&gt;').map(x => x.trim()),
+      condition: getFeedValueByKey('condition', feedProduct),
+      base_price: getFeedValueByKey('price', feedProduct.replace('/[a-z A-Z]/g', '').trim()),
+      price: getFeedValueByKey('sale_price', feedProduct.replace('/[a-z A-Z]/g', '').trim()),
+      weight: {
+        value: getFeedValueByKey('shipping_weight', feedProduct).split(' ')[0],
+        unit: getFeedValueByKey('shipping_weight', feedProduct).split(' ')[1]
+      },
+
     }
 
     return product
