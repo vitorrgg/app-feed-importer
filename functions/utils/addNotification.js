ca1dbbf4264c3071a50f48d32e862fab811b567d
Thiago Abreu Rodrigues <thiago.ar17@gmail.com>
2022-01-11 17:07:07 -0300

    adjusted addNotification parser store_id

--- functions/utils/addNotification.js
+++ functions/utils/addNotification.js
@@ -3,6 +3,9 @@ const functions = require('firebase-functions')
 module.exports = (admin, trigger) => {
   trigger.attempts = 0
   trigger.ready_at = admin.firestore.Timestamp.now().toMillis() + 500
+  if (typeof trigger.store_id !== 'number') {
+    trigger.store_id = parseInt(trigger.store_id, 10)
+  }
   functions.logger.info('[addNotification]', trigger)
   return admin.firestore()
     .collection('ecom_notifications')
